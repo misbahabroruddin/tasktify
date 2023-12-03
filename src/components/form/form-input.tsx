10194911e7ebe7a0b450d76ad4b37882a91043cf
misbahabroruddin <misbahabroruddin@gmail.com>
2023-12-03 22:06:07 +0700

    feat: add component form input

--- src/components/form/form-input.tsx
+++ src/components/form/form-input.tsx
@@ -47,7 +47,7 @@ export const FormInput = forwardRef<HTMLInputElement, FormInputProps>(
               htmlFor={id}
               className='text-xs font-semibold text-neutral-700'
             >
-              label
+              {label}
             </Label>
           ) : null}
           <Input
