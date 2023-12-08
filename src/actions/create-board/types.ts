f09b70ccafd8782c2cc6f577aa3d8324bdd9eccd
misbahabroruddin <misbahabroruddin@gmail.com>
2023-12-08 17:27:05 +0700

    feat: add actions for create board

---
+++ src/actions/create-board/types.ts
@@ -0,0 +1,8 @@
+import { ActionState } from '@/lib/create-safe-action';
+import { Board } from '@prisma/client';
+
+import { CreateBoard } from './schema';
+import { z } from 'zod';
+
+export type InputType = z.infer<typeof CreateBoard>;
+export type ReturnType = ActionState<InputType, Board>;
