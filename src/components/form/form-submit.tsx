50c554f7ecb1a3fbc7b887cf5f58e4c42bbd074d
misbahabroruddin <misbahabroruddin@gmail.com>
2023-12-11 13:00:29 +0700

    feat: set default value variant

--- src/components/form/form-submit.tsx
+++ src/components/form/form-submit.tsx
@@ -1,29 +1,29 @@
-'use client';
+"use client";
 
-import { useFormStatus } from 'react-dom';
+import { useFormStatus } from "react-dom";
 
-import { cn } from '@/lib/utils';
-import { Button } from '@/components/ui/button';
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
 
 interface FormSubmitProps {
   children: React.ReactNode;
   disabled?: boolean;
   className?: string;
   variant?:
-    | 'default'
-    | 'destructive'
-    | 'outline'
-    | 'secondary'
-    | 'ghost'
-    | 'link'
-    | 'primary';
+    | "default"
+    | "destructive"
+    | "outline"
+    | "secondary"
+    | "ghost"
+    | "link"
+    | "primary";
 }
 
 export function FormSubmit({
   children,
   disabled,
   className,
-  variant,
+  variant = "primary",
 }: FormSubmitProps) {
   const { pending } = useFormStatus();
 
