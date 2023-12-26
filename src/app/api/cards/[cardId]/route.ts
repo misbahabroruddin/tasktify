3efe887b4c503776ab1a2d38d9bbe6bb02237bba
misbahabroruddin <misbahabroruddin@gmail.com>
2023-12-26 17:36:59 +0700

    fix: unused arguments

--- src/app/api/cards/[cardId]/route.ts
+++ src/app/api/cards/[cardId]/route.ts
@@ -3,7 +3,7 @@ import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
 export async function GET(
-  req: Request,
+  _req: Request,
   { params }: { params: { cardId: string } }
 ) {
   try {
